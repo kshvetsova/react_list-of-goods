c33a4e988579fd7d98b62d5558d1204abb6dc12e
kshvetsova <katshvetsova@gmail.com>
2020-11-02 17:16:30 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.c0f927bb220f8b3d16ee68e88c4866f6.js"
 );
 
 self.addEventListener('message', (event) => {
